45743a2dff67c8c3457a770720d14eb2e39a9cb6
Artemsdrk <98228836+Artemsdrk@users.noreply.github.com>
2022-06-29 23:04:54 +0300

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,15 +1,15 @@
 from pyowm.owm import OWM
 from pyowm.utils.config import get_default_config
 
-place = input('Укажите город:')
+place = input('Specify city:')
 config_dict = get_default_config()
-config_dict['language'] = 'ru'
+config_dict['language'] = 'en'
 owm = OWM('4b9ea152afe596ebd2d2f89f9084d3f8', config_dict)
 
 mgr = owm.weather_manager()
-observation = mgr.weather_at_place(place)#Локация в которой надо узнать погоду 
+observation = mgr.weather_at_place(place)
 w = observation.weather
-temperature = w.temperature('celsius')['temp']#Показывает температуру в цельсиях 
-pogoda = w.detailed_status #Показывает погоду на данный момент 
+temperature = w.temperature('celsius')['temp']
+pogoda = w.detailed_status 
 
-print ('В городе ' + place + ' сейчас : ' + str(temperature) + ' градусов.\nВ городе ' + pogoda + '.\nХорошего дня!')
+print ('In city' + place + ' now : ' + str(temperature) + ' degrees.\nIn city ' + pogoda + '.\nGood day!')
